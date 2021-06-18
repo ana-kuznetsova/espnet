0d16b7046985441fa716f62b6197465ac1875298
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-06-18 12:08:12 -0400

    exhausted fix

--- espnet2/curriculum/curriculum_generator.py
+++ espnet2/curriculum/curriculum_generator.py
@@ -48,7 +48,7 @@ class EXP3SCurriculumGenerator(AbsCurriculumGenerator):
 
     def get_next_task_ind(self, **kwargs):
         arr = np.arange(self.K)
-        if exhausted is not None:
+        if exhausted!=None:
             #If one of the tasks is exhausted, use only those that still have data
             ind = [i for i in range(self.K) if i!=k]
             task_ind = np.random.choice(arr[ind], size=1, p=self.policy[ind])
