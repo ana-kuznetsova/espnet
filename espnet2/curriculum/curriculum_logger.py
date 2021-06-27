49f02051a834e4ec026cbdddb7185d8896b50035
anuragkumar95 <anukr95@gmail.com>
2021-06-27 15:56:47 -0400

    debug

--- espnet2/curriculum/curriculum_logger.py
+++ espnet2/curriculum/curriculum_logger.py
@@ -43,7 +43,7 @@ class CurriculumLogger:
                 fo.write(stats + '\n')
         with open(self.policy_path, 'a+') as fo:
             #fo.write(str(iepoch)+', '+str(iiter)+', '+policy.tostring()+'\n')
-            fo.write(str(iepoch)+', '+str(iiter)+', '+kwargs["policy"].tostring()+'\n')
+            fo.write(str(iepoch)+', '+str(iiter)+', '+kwargs["policy"].tostring().decode()+'\n')
         
         if kwargs["log_wandb"]:
             log_dict = {"loss":kwargs["losses"][1],
