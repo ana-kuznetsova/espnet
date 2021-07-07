f96faad3fce247463fa52f8f174948a1fe6678cb
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-07-07 15:13:01 -0400

    add vpg calculation

--- espnet2/train/trainer.py
+++ espnet2/train/trainer.py
@@ -969,6 +969,9 @@ class Trainer:
                 iterator_stop.fill_(1)
                 torch.distributed.all_reduce(iterator_stop, ReduceOp.SUM)
         logging.info(f"Finished epoch {iepoch}")
+
+        if options.gain_type=="VPG":
+            return all_steps_are_invalid, iterator, valid_iterator, tasks, valid_tasks
         return all_steps_are_invalid, iterator, tasks
 
     @classmethod
