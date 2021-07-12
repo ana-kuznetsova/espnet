8a677216da84ce96aaaae9c84108785e74db2fdc
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-07-12 17:57:50 -0400

    debug

--- espnet2/curriculum/curriculum_generator.py
+++ espnet2/curriculum/curriculum_generator.py
@@ -250,7 +250,7 @@ class SWUCBCurriculumGenerator(AbsCurriculumGenerator):
         if restore:
             self.log_dir = log_dir
             #Read history files, restore the last iter from iepoch
-            generator_state = np.load(os.path.join(self.log_dir, "generator_state.npy"),
+            generator_state = np.load(os.path.join(self.log_dir, "generator_state_"+kwargs['iepoch']+".npy"),
                                       allow_pickle=True).item()
 
             self.policy = generator_state["policy"]
