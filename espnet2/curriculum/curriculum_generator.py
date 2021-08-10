06eacde5b61c169bd7d5788736f1cc0e85f7e8dc
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-08-10 17:21:30 -0400

    debug

--- espnet2/curriculum/curriculum_generator.py
+++ espnet2/curriculum/curriculum_generator.py
@@ -445,7 +445,7 @@ class ManualCurriculumGenerator(AbsCurriculumGenerator):
                                       allow_pickle=True).item()
 
           
-            self.policy=generator_state["policy"][0], 
+            self.policy=generator_state["policy"], 
             self.epochs_per_stage=generator_state["epochs_per_stage"],
             self.start_i=generator_state["start_i"],
             self.end_i=generator_state["end_i"],
