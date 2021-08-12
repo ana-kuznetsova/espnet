4672a4ec64ea055a725c3d92204367d7ec0815bd
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-08-12 15:05:02 -0400

    calc word norms script

--- espnet2/curriculum/norm_complexity.py
+++ espnet2/curriculum/norm_complexity.py
@@ -34,3 +34,41 @@ def train_vector_model(subword_model, text, save_file, sep='\t'):
     word_vectors = model.wv
     word_vectors.save(save_file)
     print(f"Saved vectors to {save_file}.")
+    return word_vectors, training_data
+
+
+def calculate_sent_norms(vectors_file, subword_model, text, save_file):
+    sp = spm.SentencePieceProcessor()
+    sp.Load(subword_model)
+
+    wv = KeyedVectors.load(vectors_file, mmap='r')
+
+    data_dict = {}
+    print("Reading text data...")
+    with open(text, 'r') as fo:
+        for line in fo.readlines():
+            data_dict[line.split(sep)[0]] = line.split(sep)[-1].strip()
+
+    word_norms = {}
+
+    for k in tqdm(data_dict):
+        sent = data_dict[k]
+        sent = sp.EncodeAsPieces(sent) + ['▁']
+        token = ''
+        norm = 0
+        for j, sub_word in enumerate(sent):
+            if '▁' in sub_word and len(token) > 0:
+                token = token.replace('▁', '')
+                word_norms[token] = norm
+                token = ''
+                token+=sub_word
+                norm = 0
+                norm += np.linalg.norm(wv[sub_word])
+            else:
+                token += sub_word
+                norm += np.linalg.norm(wv[sub_word]) 
+    
+    with open(save_file, 'w') as fo:
+        for k in word_norms:
+            fo.write(k+' '+str(word_norms[k])+'\n')
+    print(f"Saved word norms to {save_file}.")
