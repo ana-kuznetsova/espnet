54a858061f425ebcfa63468472e9344bc3c8886d
Anurag Kumar <anurag.kumar@rev.com>
2021-08-25 11:05:31 -0400

    added multithreading compression ratios

--- espnet2/curriculum/compression_ratio.py
+++ espnet2/curriculum/compression_ratio.py
@@ -3,8 +3,9 @@ import pandas as pd
 import subprocess
 from tqdm import tqdm
 import argparse
+import threading
 """
-def calc_CR(wav_scp, data_dir, res_dir):
+def calc_CR_wav(wav_scp, data_dir, res_dir):
 
     with open(wav_scp) as fo:
         commands = fo.readlines()
@@ -13,7 +14,7 @@ def calc_CR(wav_scp, data_dir, res_dir):
         for cmd in tqdm(commands):
             print("Command:", cmd)
             fname = cmd.split()[0]
-            fpath = os.path.join(data_dir, "/".join(fname.split('_')[:-1]), fname)
+            fpath = os.path.join(data_dir, "/".join(fname.split('-')[:-1]), fname)
             print("fpath:", fpath)
             cmd_convert = cmd.split()[1:-2]
             print(cmd_convert)
@@ -33,38 +34,82 @@ def calc_CR(wav_scp, data_dir, res_dir):
             temp = subprocess.run(["rm", fname_out+".wav"+".gz"])
             temp = subprocess.run(["rm", fname_out+".wav"])
             CR = 1 - (fsize_comp/fsize)
-
             fo.write(fname+' '+str(CR)+"\n")
 """
-def calc_CR(data_dir, res_dir):
+def calc_CR(data_dir, res_dir, map_, file_, start=None, end=None):
     p = os.path.join(data_dir,'clips')
-    train_csv = os.path.join(data_dir, 'train.tsv')
-    #data = pd.read_csv(train_csv, sep='\t')['path']
-    data = pd.read_csv(train_csv, sep='\t')
+    files = map_
+    data = file_[start:end]
+    for x in tqdm(data.iterrows()):
+        ind, row = x
+        fname = row['path']
+        client = row['client_id']
+        fname_in = os.path.join(p, fname)
+        temp = subprocess.run(["gzip", "-k", fname_in])
+        fsize = subprocess.run(["du", fname_in], stdout=subprocess.PIPE, 
+                                            text=True, check=True)
+        fsize_comp = subprocess.run(["du", fname_in+'.gz'], stdout=subprocess.PIPE, 
+                                            text=True, check=True)
+        fsize = int(fsize.stdout.split('\t')[0])
+        fsize_comp = int(fsize_comp.stdout.split('\t')[0])
+        temp = subprocess.run(["rm", fname_in+".gz"])
+        CR = 1 - (fsize_comp/fsize)
+        files[client+'-'+fname.split('.')[0]] = str(CR)
+
+
+def save_file(map_, res_dir, wav_scp=None):  
     with open(os.path.join(res_dir, "compression_ratio"), 'w') as fo:
-        for x in tqdm(data.iterrows()):
-            ind, row = x
-            fname = row['path']
-            client = row['client_id']
-            fname_in = os.path.join(p, fname)
-            temp = subprocess.run(["gzip", "-k", fname_in])
-            fsize = subprocess.run(["du", fname_in], stdout=subprocess.PIPE, 
-                                                text=True, check=True)
-            fsize_comp = subprocess.run(["du", fname_in+'.gz'], stdout=subprocess.PIPE, 
-                                                text=True, check=True)
-            fsize = int(fsize.stdout.split('\t')[0])
-            fsize_comp = int(fsize_comp.stdout.split('\t')[0])
-            temp = subprocess.run(["rm", fname_in+".gz"])
-            CR = 1 - (fsize_comp/fsize)
-            fo.write(client+'-'+fname.split('.')[0]+' '+str(CR)+"\n")
-            #fo.write('sp0.9-'+client+'-'+fname.split('.')[0]+' '+"["+str(CR)+"]\n")
-            #fo.write('sp1.1-'+client+'-'+fname.split('.')[0]+' '+"["+str(CR)+"]\n")
+        if wav_scp:
+            fe = open('extras', 'w') 
+            print("Comapring wav_scp files.....")
+            for line in open(wav_scp,'r').readlines():
+                fname = line.split()[0]
+                if fname in files:
+                    fo.write(fname + ' ' + map_[fname]+'\n')
+                else:
+                    fe.write(fname)
+            fe.close()
+        else:
+            for file in map_:
+                fo.write(file + ' ' + map_[file]+'\n')
+
+                #fo.write('sp0.9-'+client+'-'+fname.split('.')[0]+' '+"["+str(CR)+"]\n")
+                #fo.write('sp1.1-'+client+'-'+fname.split('.')[0]+' '+"["+str(CR)+"]\n")
             
+def main(args):
+    map_ = {}
+    if not num_threads:
+        calc_CR(args.data_dir, args.res_dir, map_) 
+    else:
+        threads = []
+        for file_ in ['validated.tsv', 'invalidated.tsv']: 
+            csv = os.path.join(args.data_dir, file_)
+            csv_len = len(csv)
+            rows_per_thread = csv_len/args.num_threads
+            for i in range(args.num_threads):
+                t = threading.Thread(target=calc_CR, args=(args.data_dir, 
+                                                           args.res_dir, 
+                                                           map_, 
+                                                           file_=csv, 
+                                                           i*rows_per_thread,
+                                                           min((i+1)*rows_per_thread, csv_len)))
+                threads.append(t)
+                print(f"starting thread {i} for file {file_}")
+                t1.start()
+        
+        for thread in threads:
+            thread.join()
+
+    if args.wav_scp:
+        save_file(map_, args.res_dir, args.wav_scp)
+    else:
+        save_file(map_, args.res_dir)
 
 
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--num_threads", type=str, required=False)
     parser.add_argument('--wav_scp', type=str, required=False)
     parser.add_argument('--data_dir', type=str, required=True,
                         help='Path to audio dir.')
@@ -72,4 +117,4 @@ if __name__=="__main__":
                         help='Path to dir where csv with the results will be stored.')
 
     args = parser.parse_args()
-    calc_CR(args.data_dir, args.res_dir)
+    main(args)
