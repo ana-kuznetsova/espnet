0176956f741f023d2f111ce3fd2b722ea57dddef
Anurag Kumar <anurag.kumar@rev.com>
2021-09-06 17:35:02 -0400

    debug

--- espnet2/tasks/abs_task.py
+++ espnet2/tasks/abs_task.py
@@ -804,7 +804,7 @@ class AbsTask(ABC):
 
         group.add_argument(
             "--shared_array",
-            type=list[tensor],
+            type=list,
             default=None,
             help="Lock for synchronization",
         )
