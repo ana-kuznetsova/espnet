d7c41dea25880356e9ab41135f9a3d58d6253265
Anurag Kumar <anurag.kumar@rev.com>
2021-09-06 15:35:03 -0400

    added multigpu curriculum

--- espnet2/train/trainer.py
+++ espnet2/train/trainer.py
@@ -1040,7 +1040,7 @@ class Trainer:
                 l1, l2 = 0, 0
                 f = open('temp.losses', 'r')
                 lines = f.readlines()
-                print(len(lines))
+                log.info(f"lines:{len(lines)}")
                 if len(lines) == ngpu:
                     for line in lines:
                         l1 += float(line.split()[0])
