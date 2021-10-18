c703f0d68ee1bef977fcc7182177222c899332c1
Anurag Kumar <anurag.kumar@rev.com>
2021-10-18 18:23:39 -0500

    .

--- espnet2/curriculum/compression_ratio.py
+++ espnet2/curriculum/compression_ratio.py
@@ -68,12 +68,14 @@ def compress_file(map_, wav_id, name, file_path, save_path):
     temp = subprocess.run(["rm", save_path+".gz"])
 
 def calc_CR_scp(pid, map_, file_, type, segments=None, start=None, end=None):
+    print(pid)
     tqdm_text = "#"+"{}".format(pid).zfill(3)
     data = file_[start:end]
     if segments:
         segments = pd.read_csv(segments, sep = ' ', header=None)
     with tqdm(total=end-start, desc=tqdm_text, position=pid+1) as pbar:
         for idx, row in data.iterrows():
+            print("sssss")
             wav_id = row[0]
             fpath = row[6]
             filename = fpath.split('/')[-1]
