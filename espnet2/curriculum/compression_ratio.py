568d9c6de92907a1e052370aeac88afe57d42ae2
Anurag Kumar <anurag.kumar@rev.com>
2021-10-22 11:36:49 -0500

    .

--- espnet2/curriculum/compression_ratio.py
+++ espnet2/curriculum/compression_ratio.py
@@ -89,8 +89,6 @@ def calc_CR_scp(pid, map_, file_, args, segments=None, start=None, end=None):
                 fpath = save_path
             if isinstance(segments, pd.DataFrame):
                 segs = segments[segments[0] == wav_id]
-                print(wav_id)
-                print(segments.head())
                 compress_segments(map_=map_, 
                                   wav_id=wav_id,
                                   file_path=fpath,
