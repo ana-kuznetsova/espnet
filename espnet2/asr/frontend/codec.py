a7cf53adf8ceebb8c0b04024ce2a25fee38becc9
Anastasia  Kuznetsova <menina.indigena.17@gmail.com>
2023-09-06 22:22:57 -0400

    add bitrate options to codec frontend

--- espnet2/asr/frontend/codec.py
+++ espnet2/asr/frontend/codec.py
@@ -5,12 +5,18 @@ from espnet2.asr.frontend.abs_frontend import AbsFrontend
 
 class CodecFrontend(AbsFrontend):
     '''DAC speech codec frontend.
+    Args:
+        fs: sampling frequency of the codec
+        n_quantizers: desired number of quantizers for a specific bitrate
+        trainable: freeze codec or not
     '''
     def __init__(self, fs: int = 16000,
+                       n_quantizers: int = 6,
                        trainable: bool = False) -> None:
         super().__init__()
         self.fs = fs
         self.feat_dim = 1024
+        self.n_quantizers = n_quantizers
         codec_path = dac.utils.download(model_type="16khz")
         self.codec = dac.DAC.load(codec_path)
         if trainable:
@@ -24,7 +30,7 @@ class CodecFrontend(AbsFrontend):
     def forward(self, input: torch.Tensor, input_lengths: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         #print("Inp len", input_lengths)
         input = input.unsqueeze(1)
-        z, _, _, _, _ = self.codec.encode(input)
+        z, _, _, _, _ = self.codec.encode(input, self.n_quantizers)
         # Convert input to (B, L, Dim)
         bsize, feat_dim, length = z.size()
         z = z.view(bsize, length, feat_dim)
