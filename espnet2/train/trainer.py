f3deaee73434f487105a4b25c7802d89105d87ba
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2023-09-10 17:56:01 -0400

    debug labels

--- espnet2/train/trainer.py
+++ espnet2/train/trainer.py
@@ -564,7 +564,9 @@ class Trainer:
             ):
                 with reporter.measure_time("forward_time"):
                     retval = model(**batch)
-
+                    print('---------------------------')
+                    print("DEBUG Batch", batch )
+                    print('---------------------------')
                     # Note(kamo):
                     # Supporting two patterns for the returned value from the model
                     #   a. dict type
