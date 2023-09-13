b2cae31962d4492e9e2a8eefdab921b42f7b8a13
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2023-09-13 11:26:50 -0400

    debug codec preproc

--- espnet2/asr/frontend/codec.py
+++ espnet2/asr/frontend/codec.py
@@ -33,6 +33,7 @@ class CodecFrontend(AbsFrontend):
     def forward(self, input: torch.Tensor, input_lengths: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         #print("Inp len", input_lengths)
         input = input.unsqueeze(1)
+        print("DEBUG CODEC preproc", self.fs, self.codec.sample_rate)
         input = self.codec.preprocess(input, self.fs)
         z, _, _, _, _ = self.codec.encode(input, self.n_quantizers)
         # Convert input to (B, L, Dim)
