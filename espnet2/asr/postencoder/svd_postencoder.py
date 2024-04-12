d52cabab52b0670f3cfba816f14e1043a930e166
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2024-04-12 13:52:14 -0400

    add svd post_encoder

---
+++ espnet2/asr/postencoder/svd_postencoder.py
@@ -0,0 +1,31 @@
+"""SVD post encoder for reducing data rate."""
+
+from typing import Optional, Tuple
+
+import torch
+from typeguard import check_argument_types
+
+from espnet2.asr.postencoder.abs_postencoder import AbsPostEncoder
+
+class SVDPostencoder(AbsPostEncoder):
+    def __init__(
+        self,
+        input_size: int,
+        svd_dim:int, 
+        output_size: Optional[int] = None, 
+    ):
+        """Initialize the module."""
+        assert check_argument_types()
+        super().__init__()
+        self.input_size = input_size
+        self.svd_dim = svd_dim
+        self.output_size = output_size
+    
+    def forward(self, input: torch.Tensor, input_lengths: torch.Tensor) -> Tuple[torch.Tensor]:
+        U, S, VT = torch.linalg.svd(input)
+        S = torch.diag(S)
+        output = U[:, :self.svd_dim] @ S[:self.svd_dim, :self.svd_dim] @ VT[:self.svd_dim]
+        return output, input_lengths
+
+    def output_size(self) -> int:
+        return self.output_size
--- espnet2/tasks/asr.py
+++ espnet2/tasks/asr.py
@@ -64,6 +64,7 @@ from espnet2.asr.postencoder.abs_postencoder import AbsPostEncoder
 from espnet2.asr.postencoder.hugging_face_transformers_postencoder import (
     HuggingFaceTransformersPostEncoder,
 )
+from espnet2.asr.postencoder.svd_postencoder import SVDPostencoder
 from espnet2.asr.postencoder.length_adaptor_postencoder import LengthAdaptorPostEncoder
 from espnet2.asr.preencoder.abs_preencoder import AbsPreEncoder
 from espnet2.asr.preencoder.linear import LinearProjection
@@ -172,6 +173,7 @@ postencoder_choices = ClassChoices(
     classes=dict(
         hugging_face_transformers=HuggingFaceTransformersPostEncoder,
         length_adaptor=LengthAdaptorPostEncoder,
+        svd=SVDPostencoder,
     ),
     type_check=AbsPostEncoder,
     default=None,
